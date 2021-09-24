fd3c77fbe32e9986581430df49d737abbe93c16b
Scott Blechman <14227901+scottblechman@users.noreply.github.com>
2021-09-23 23:42:03 -0500

    Initial Commit

---
+++ Fetchling/FetchlingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchlingApp.swift
+//  Fetchling
+//
+//  Created by Scott Blechman on 9/23/21.
+//
+
+import SwiftUI
+
+@main
+struct FetchlingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
